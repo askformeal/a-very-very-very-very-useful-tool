4ad04019e2c6ca99f8f413be82e7f2b8abd1d6be
Li Muzhi <109349321+askformeal@users.noreply.github.com>
2022-09-08 20:34:43 +0800

    Add files via upload

---
+++ playground.py
@@ -0,0 +1,10 @@
+def check_prime_number(number,AC=False,DFE=0,COSE=1):
+    numbers = list(range(2,number))
+    for n in numbers:
+        if number%n == 0:
+            return False
+
+    return True
+
+print(check_prime_number(7))
+print(check_prime_number(9))
